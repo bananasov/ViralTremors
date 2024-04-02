a390e1002681bc0129f90db98b9575d9d349faf9
sovnya <enp7s1@tutanota.com>
2024-04-02 19:12:01 +0200

    fix: Actually implement weeping enemy vibration

--- ViralTremors/Patches/Bot_Weeping.cs
+++ ViralTremors/Patches/Bot_Weeping.cs
@@ -12,6 +12,11 @@ public class Bot_WeepingPatches
     // ReSharper disable once InconsistentNaming
     private static void TryCapturePlayer(Bot_Weeping __instance)
     {
-        
+        ViralTremors.Mls.LogDebug($"GoUnderground got called");
+
+        if (ViralTremors.DeviceManager.IsConnected() && Config.WeepingEnemyCaptureEnabled.Value)
+        {
+            ViralTremors.DeviceManager.VibrateConnectedDevicesWithDuration(Config.WeepingEnemyCaptureStrength.Value, Config.WeepingEnemyCaptureDuration.Value);
+        }
     }
 }
