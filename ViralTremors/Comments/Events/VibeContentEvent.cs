65bab9efa5adda35ee0b4163705d6bfc98c09beb
sovnya <enp7s1@tutanota.com>
2024-04-08 10:23:57 +0200

    feat: Add basic comment template

---
+++ ViralTremors/Comments/Events/VibeContentEvent.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using Zorro.Core;
+using Zorro.Core.Serizalization;
+
+namespace ViralTremors.Comments.Events;
+
+public class VibeContentEvent : ContentEvent
+{
+    // I am well aware i have basically reimplemented PlayerBaseEvent.
+    public VibeContentEvent(string playerName, int actorNumber, float strength, float duration)
+    {
+        Strength = strength;
+        Duration = duration;
+        PlayerName = playerName;
+        ActorNumber = actorNumber;
+    }
+    
+    public override float GetContentValue()
+    {
+        return 1000f; // Pretty fucking big.
+    }
+    
+    public override Comment GenerateComment()
+    {
+        return new Comment(FixPlayerName(VIBE_COMMENTS.GetRandom()));
+    }
+
+    public override int GetUniqueID()
+    {
+        return ActorNumber;
+    }
+
+    public override ushort GetID()
+    {
+        return 5601;
+    }
+
+    public override string GetName()
+    {
+        return "PlayerVibed";
+    }
+    
+    public override void Serialize(BinarySerializer serializer)
+    {
+        serializer.WriteInt(ActorNumber);
+        serializer.WriteString(PlayerName, Encoding.UTF8);
+        serializer.WriteFloat(Strength);
+        serializer.WriteFloat(Duration);
+    }
+
+    public override void Deserialize(BinaryDeserializer deserializer)
+    {
+        ActorNumber = deserializer.ReadInt();
+        PlayerName = deserializer.ReadString(Encoding.UTF8);
+        Strength = deserializer.ReadFloat();
+        Duration = deserializer.ReadFloat();
+    }
+    
+    public string FixPlayerName(string comment)
+    {
+        return comment.Replace("<playername>", this.PlayerName);
+    }
+
+    public string PlayerName;
+    public int ActorNumber;
+    public float Strength;
+    public float Duration;
+
+    public string[] VIBE_COMMENTS =
+    {
+        "Is <playername> using a vibrator!?",
+        "Did <playername> just moan??",
+        "Wow! Seems like <playername> is having a LOT of fun!",
+        "Wish i was controlling their toy.",
+        "Just banged <playername>.",
+    };
+}
---
+++ ViralTremors/Comments/Providers/VibedContentProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using ViralTremors.Comments.Events;
+
+namespace ViralTremors.Comments.Providers;
+
+public class VibedContentProvider : ContentProvider
+{
+    public VibedContentProvider(float strength, float duration, Player player)
+    {
+        _strength = strength;
+        _duration = duration;
+        this.player = player;
+    }
+    
+    public override void GetContent(List<ContentEventFrame> contentEvents, float seenAmount, Camera camera, float time)
+    {
+        var nickName = player.refs.view.Owner.NickName;
+        var actorNumber = player.refs.view.Owner.ActorNumber;
+        
+        contentEvents.Add(new ContentEventFrame(new VibeContentEvent(nickName, actorNumber, _strength, _duration), seenAmount, time));
+    }
+
+    private readonly float _strength;
+    private readonly float _duration;
+    public Player player;
+}
